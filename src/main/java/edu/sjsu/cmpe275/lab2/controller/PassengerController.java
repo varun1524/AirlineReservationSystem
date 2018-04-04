2f4d176536692f087bcea576ef1ef6358bd13bb3
varun1524 <varun3273@gmail.com>
2018-04-04 02:02:24 -0700

    passenger entity and service update

--- src/main/java/edu/sjsu/cmpe275/lab2/controller/PassengerController.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/controller/PassengerController.java
@@ -5,10 +5,9 @@ import edu.sjsu.cmpe275.lab2.service.PassengerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/passenger")
@@ -17,16 +16,41 @@ public class PassengerController {
     @Autowired
     PassengerService passengerService;
 
-    @GetMapping(path = "test")
-    public ResponseEntity test(){
+    @PostMapping
+    public ResponseEntity addPassenger(@RequestParam Map params){
         Passenger p = null;
+        HttpStatus status = null;
         try{
-            p = passengerService.findByPassengerId(1);
-            System.out.println(p.getFirstname());
+            p = passengerService.addPassenger(params);
+            if(p==null){
+                status = HttpStatus.NOT_FOUND;
+            }
+            else {
+                status = HttpStatus.OK;
+            }
         }
         catch (Exception e){
             e.printStackTrace();
         }
-        return new ResponseEntity(p, HttpStatus.OK);
+        return new ResponseEntity(p, status);
+    }
+
+    @GetMapping(path = "{id}")
+    public ResponseEntity addPassenger(@PathVariable String id){
+        Passenger p = null;
+        HttpStatus status = null;
+        try{
+            p = passengerService.findByPassengerId(id);
+            if(p==null){
+                status = HttpStatus.NOT_FOUND;
+            }
+            else {
+                status = HttpStatus.OK;
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity(p, status);
     }
 }
--- src/main/java/edu/sjsu/cmpe275/lab2/service/PassengerService.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/service/PassengerService.java
@@ -6,17 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PassengerService {
     @Autowired
     PassengerRepository passengerRepository;
 
-    public Passenger save(Passenger passenger){
-        return passengerRepository.save(passenger);
+    public Passenger addPassenger(Map params){
+        Passenger passenger = null;
+        try{
+            Passenger passenger1 = new Passenger();
+            passenger1.setPhone(params.get("phone").toString());
+            passenger1.setAge(Integer.parseInt(params.get("age").toString()));
+            passenger1.setGender(params.get("gender").toString());
+            passenger1.setFirstname(params.get("firstname").toString().toLowerCase());
+            passenger1.setLastname(params.get("lastname").toString().toLowerCase());
+            passenger = passengerRepository.save(passenger1);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return passenger;
     }
 
-    public Passenger findByPassengerId(int id){
+    public Passenger findByPassengerId(String id){
         return passengerRepository.findByPassengerId(id);
     }
 
