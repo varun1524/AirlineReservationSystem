e5ba8eedaff800dfb6709fd4e642e1854300541c
varun1524 <varun3273@gmail.com>
2018-04-03 18:30:33 -0700

    Repository name corrected

---
+++ src/main/java/edu/sjsu/cmpe275/lab2/repository/ReservationRepository.java
@@ -0,0 +1,16 @@
+package edu.sjsu.cmpe275.lab2.repository;
+
+import edu.sjsu.cmpe275.lab2.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, String> {
+
+    List<Reservation> findAll();
+
+    List<Reservation> findByReservationNumber(int reservationNumber);
+
+}
