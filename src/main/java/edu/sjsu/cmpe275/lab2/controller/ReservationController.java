e0be140340fd26a7bd6f680a722aea699f70f99a
varun1524 <varun3273@gmail.com>
2018-04-04 04:14:18 -0700

    GET mapping to fetch reservation details by reservationnumber implemented

--- src/main/java/edu/sjsu/cmpe275/lab2/controller/ReservationController.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/controller/ReservationController.java
@@ -1,19 +1,15 @@
 package edu.sjsu.cmpe275.lab2.controller;
 
 import edu.sjsu.cmpe275.lab2.entity.Reservation;
-import edu.sjsu.cmpe275.lab2.service.FlightService;
-import edu.sjsu.cmpe275.lab2.service.PassengerService;
-import edu.sjsu.cmpe275.lab2.service.PlaneService;
 import edu.sjsu.cmpe275.lab2.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -24,32 +20,28 @@ public class ReservationController {
     ReservationService reservationService;
 
 
-
-    @GetMapping(path = "fetchReservationById")
-    public ResponseEntity fetchReservationById(){
-        List<Reservation> list = Collections.emptyList();
+    @PostMapping
+    public ResponseEntity makeReservation(@RequestParam Map<String, String> params){
+        ResponseEntity responseEntity = null;
         try{
-
-            /*List<Flight> flightList = new LinkedList<>();
-            flightList.add(flightService.findByFlightNumber("123"));
-            flightList.add(flightService.findByFlightNumber("124"));
-
-            Reservation reservation = new Reservation();
-            reservation.setFlights(flightList);
-            reservation.setPassenger(passengerService.findByPassengerId(1));
-            double price=0;
-            for(Flight flight : flightList){
-                price += flight.getPrice();
-            }
-            System.out.println("price: " + price);
-            reservation.setPrice(price);
-            Reservation reservation1 = reservationService.save(reservation);
-            System.out.println(reservation1);*/
-            list = reservationService.findReservationsByID(1);
+            responseEntity = reservationService.makeReservation(params);
         }
         catch (Exception e){
             e.printStackTrace();
         }
-        return new ResponseEntity(list, HttpStatus.OK);
+        return responseEntity;
+    }
+
+
+    @GetMapping(path = "/{reservationNumber}")
+    public ResponseEntity fetchReservationById(@PathVariable String reservationNumber){
+        ResponseEntity responseEntity = null;
+        try{
+            responseEntity = reservationService.findReservationsByID(reservationNumber);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return responseEntity;
     }
 }
