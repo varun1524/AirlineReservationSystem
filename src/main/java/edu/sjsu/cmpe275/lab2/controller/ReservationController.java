9718a401413479ceaef5b16dcc193fc37eef0cd6
varun1524 <varun3273@gmail.com>
2018-04-06 20:55:42 -0700

    Make Reservation functinality implemented

--- src/main/java/edu/sjsu/cmpe275/lab2/controller/ReservationController.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/controller/ReservationController.java
@@ -1,14 +1,10 @@
 package edu.sjsu.cmpe275.lab2.controller;
 
-import edu.sjsu.cmpe275.lab2.entity.Reservation;
 import edu.sjsu.cmpe275.lab2.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 
@@ -18,7 +14,6 @@ public class ReservationController {
 
     @Autowired
     ReservationService reservationService;
-
 
     @PostMapping
     public ResponseEntity makeReservation(@RequestParam Map<String, String> params){
@@ -31,7 +26,6 @@ public class ReservationController {
         }
         return responseEntity;
     }
-
 
     @GetMapping(path = "/{reservationNumber}")
     public ResponseEntity fetchReservationById(@PathVariable String reservationNumber){
