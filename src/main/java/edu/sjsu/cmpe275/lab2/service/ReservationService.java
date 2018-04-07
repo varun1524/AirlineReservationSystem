4d5ed1513b34bd0c3261cc69e35ee7df8118fca3
varun1524 <varun3273@gmail.com>
2018-04-06 20:53:49 -0700

    Make Reservation functinality implemented

--- src/main/java/edu/sjsu/cmpe275/lab2/service/ReservationService.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/service/ReservationService.java
@@ -7,11 +7,11 @@ import edu.sjsu.cmpe275.lab2.repository.FlightRepository;
 import edu.sjsu.cmpe275.lab2.repository.PassengerRepository;
 import edu.sjsu.cmpe275.lab2.repository.ReservationRepository;
 import org.json.JSONObject;
-import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -28,6 +28,7 @@ public class ReservationService {
     @Autowired
     PassengerRepository passengerRepository;
 
+
     public Reservation save(Reservation reservation){
         return reservationRepository.save(reservation);
     }
@@ -38,8 +39,9 @@ public class ReservationService {
 
     public ResponseEntity findReservationsByID(String reservationNumber){
         ResponseEntity responseEntity = null;
+        Reservation reservation = null;
         try{
-            Reservation reservation = reservationRepository.findByReservationNumber(reservationNumber);
+            reservation = reservationRepository.findByReservationNumber(reservationNumber);
             if(reservation!=null){
                 responseEntity = new ResponseEntity<>(reservation.getWholeReservationDetailsJSON().toString(), HttpStatus.OK);
             }
@@ -56,48 +58,152 @@ public class ReservationService {
             e.printStackTrace();
         }
         return responseEntity;
+//        return new ResponseEntity(reservation, HttpStatus.OK);
     }
 
-    public ResponseEntity makeReservation(Map<String, String> params) {
+
+    public ResponseEntity makeReservation(Map<String, String> params) throws Exception{
+        ResponseEntity responseEntity = null;
+        HttpStatus status = HttpStatus.NOT_FOUND;
         Reservation reservation = null;
+        JSONObject jsonObject = new JSONObject();
+        JSONObject jsonObject1 = new JSONObject();
+        jsonObject1.put("msg", "Failed to make reservation for Flight");
+        jsonObject1.put("code", status);
+        jsonObject.put("Bad Request",jsonObject1);
         try{
             params.entrySet().forEach(stringStringEntry -> {
-                System.out.println(stringStringEntry.getKey() + "   :  " + stringStringEntry.getValue());
+                System.out.println(stringStringEntry.getKey() + "  :  " + stringStringEntry.getValue());
             });
+
             String passengerId = params.get("passengerId");
             String[] flightArr = params.get("flightLists").split(",");
-            List<Flight> flightList = new LinkedList<>();
-
             Passenger passenger = passengerRepository.findByPassengerId(passengerId);
 
-            double price = 0;
-            Flight tempFlight = null;
-            for(String s : flightArr){
-                if((tempFlight = flightRepository.findByFlightNumber(s))!=null){
-                    price+=tempFlight.getPrice();
-                    tempFlight.setSeatsLeft(tempFlight.getSeatsLeft()-1);
-                    List<Passenger> passengers = tempFlight.getPassengers();
-                    passengers.add(passenger);
-                    tempFlight.setPassengers(passengers);
-                }
-                flightList.add(tempFlight);
-            }
+            if(passenger!=null){
+                List<Flight> newFlightForReservation = new LinkedList<>();
+                boolean isValidFlight = false;
+                for (String flightNum : flightArr) {
+                    Flight flight = flightRepository.findByFlightNumber(flightNum);
+                    if(flight!=null){
+                        newFlightForReservation.add(flight);
+                        if(flight.getSeatsLeft()>0) {
+                            List<Flight> previousBookings = passenger.getFlights();
+                            for(Flight flight1 : previousBookings){
+                                if(flight.getFlightNumber().equals(flight1.getFlightNumber())){
+                                    isValidFlight = true;
+                                    break;
+                                }
+                                else {
+                                    if((flight.getDepartureTime().compareTo(flight1.getDepartureTime())>=0 && flight.getDepartureTime().compareTo(flight1.getArrivalTime())<=0)
+                                            ||
+                                            (flight.getArrivalTime().compareTo(flight1.getDepartureTime())>=0 && flight.getArrivalTime().compareTo(flight1.getArrivalTime())<=0)){
+                                        isValidFlight = true;
+                                        break;
+                                    }
+                                }
+                            }
 
-            Reservation reservation1 = new Reservation();
-            reservation1.setFlights(flightList);
-            reservation1.setPrice(price);
-            reservation1.setPassenger(passenger);
-            reservation = reservationRepository.save(reservation1);
-            if(reservation!=null){
-                return new ResponseEntity(XML.toString(reservation), HttpStatus.OK);
+                            /*List<Flight> overlappedFlights   = flightRepository.findOverLappedFlights(
+                                    passenger.getPassengerId(),
+                                    flight.getFlightNumber(),
+                                    flight.getDepartureTime(),
+                                    flight.getArrivalTime());
+                            if(overlappedFlights.size()>0){
+                                isValidFlight = true;
+                                break;
+                            }*/
+                        }
+                        else {
+                            isValidFlight = true;
+                            break;
+                        }
+                    }
+                    else {
+                        isValidFlight = true;
+                        break;
+                    }
+                }
+                if(!isValidFlight){
+                    if(checkInterBetweenOverLapping(newFlightForReservation)) {
+                        reservation = createReservationEntry(newFlightForReservation, passenger);
+                        if(reservation!=null){
+                            jsonObject = reservation.getWholeReservationDetailsJSON();
+                            responseEntity = new ResponseEntity(jsonObject.toString(), HttpStatus.OK);
+                        }
+                        else {
+                            responseEntity = new ResponseEntity(jsonObject.toString(), HttpStatus.NOT_FOUND);
+                        }
+                    }
+                }
+                else {
+                    responseEntity = new ResponseEntity(jsonObject.toString(), HttpStatus.NOT_FOUND);
+                }
             }
-            else {
-                return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+            else{
+                responseEntity = new ResponseEntity(jsonObject.toString(), HttpStatus.NOT_FOUND);
             }
         }
         catch (Exception e){
             e.printStackTrace();
         }
-        return null;
+
+        return responseEntity;
+    }
+
+    private boolean checkInterBetweenOverLapping(List<Flight> flights){
+        boolean result = true;
+        for(Flight flight : flights){
+            for(Flight flight1 : flights){
+                if(!flight.getFlightNumber().equals(flight1.getFlightNumber())){
+                    if((flight.getDepartureTime().compareTo(flight1.getDepartureTime())>0
+                            &&
+                            flight.getDepartureTime().compareTo(flight1.getArrivalTime())<0)
+                            ||
+                            (flight.getArrivalTime().compareTo(flight1.getDepartureTime())>0
+                                    &&
+                                    flight.getArrivalTime().compareTo(flight1.getArrivalTime())<0)
+                            ){
+                        result = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    @Transactional(rollbackFor = {Exception.class})
+    public Reservation createReservationEntry(List<Flight> newflightList, Passenger passenger) throws Exception{
+        double price = 0;
+        Flight tempFlight = null;
+        List<Flight> flightList = new LinkedList<>();
+        for(Flight flight : newflightList){
+            if((tempFlight = flightRepository.findByFlightNumber(flight.getFlightNumber()))!=null){
+                price+=tempFlight.getPrice();
+                tempFlight.setSeatsLeft(tempFlight.getSeatsLeft()-1);
+                List<Passenger> passengers = tempFlight.getPassengers();
+                passengers.add(passenger);
+                tempFlight.setPassengers(passengers);
+            }
+            flightList.add(tempFlight);
+        }
+
+        Reservation reservation1 = new Reservation();
+        reservation1.setFlights(flightList);
+        reservation1.setPrice(price);
+        reservation1.setPassenger(passenger);
+        Reservation reservation = reservationRepository.save(reservation1);
+
+        if(reservation!=null){
+//                return new ResponseEntity(XML.toString(reservation.), HttpStatus.OK);
+            System.out.println("Reservation Successful");
+        }
+        else {
+//                return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+            System.out.println("Reservation Unsuccessful");
+            throw new Exception("Error. Perform RollBack");
+        }
+        return reservation;
     }
 }
