23f6240e5815ed88f4c1be52f85ab0cd4dfc1f89
varun1524 <varun3273@gmail.com>
2018-04-06 20:54:45 -0700

    Customized Query to find overlapped flights for reservation added

--- src/main/java/edu/sjsu/cmpe275/lab2/repository/FlightRepository.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/repository/FlightRepository.java
@@ -1,18 +1,31 @@
 package edu.sjsu.cmpe275.lab2.repository;
 
 import edu.sjsu.cmpe275.lab2.entity.Flight;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.NamedQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, String> {
+
     List<Flight> findAll();
 
     Flight findByFlightNumber(String flightNumber);
 
     @Transactional
     int deleteFlightByFlightNumber(String flightNumber);
+
+
+    @Query("select f from Flight f inner join f.passengers p  " +
+            " where (p.passengerId = :passengerId and " +
+            " f.flightNumber<>:flightNo and  ((f.departureTime >= :departureTime  and f.departureTime <= :arrivalTime) " +
+            " or (f.arrivalTime >= :departureTime  and f.arrivalTime <= :arrivalTime)))")
+    List<Flight> findOverLappedFlights(@Param("passengerId") String passengerId, @Param("flightNo") String flightNo, @Param("departureTime") Date departureTime, @Param("arrivalTime") Date arrivalTime);
 }
