9695ff180a6b232a2126485211ad61c1c5b2f87b
sannisthsoni <soni.sannisth@gmail.com>
2018-04-10 20:56:06 -0700

    PassengerId passing between create and delete methods

--- src/test/java/edu/sjsu/cmpe275/lab2/controller/PassengerControllerTest.java
+++ src/test/java/edu/sjsu/cmpe275/lab2/controller/PassengerControllerTest.java
@@ -7,10 +7,9 @@ import edu.sjsu.cmpe275.lab2.repository.FlightRepository;
 import edu.sjsu.cmpe275.lab2.repository.PassengerRepository;
 import edu.sjsu.cmpe275.lab2.service.FlightService;
 import edu.sjsu.cmpe275.lab2.service.PassengerService;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.json.JSONObject;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
@@ -19,11 +18,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PassengerControllerTest {
     @Autowired
     private MockMvc mockmvc;
 
-    private final String compare = "{\"flightNumber\":\"126\",\"price\":150.0,\"source\":\"Houston\",\"destination\":\"San Jose\",\"departureTime\":\"2018-05-16-20\",\"arrivalTime\":\"2018-05-17-00\",\"seatsLeft\":50,\"description\":\"Direct flight\",\"plane\":{\"capacity\":50,\"model\":\"Boeing 747\",\"manufacturer\":\"airbus\",\"year\":1993},\"passengers\":[]}";
+    public String PassengerId="";
 
     @MockBean
     @Autowired
@@ -46,10 +46,13 @@ public class PassengerControllerTest {
     // Positive test for making Passenger
     // There cannot be negative as URL will always have those parameters (as given)
     public void createPassenger() throws UnirestException {
-        String url = "http://localhost:8888/passenger?firstname=sannisth&lastname=shah&age=22&gender=male&phone=123321";
+        String url = "http://localhost:8888/passenger?firstname=sannisth&lastname=shah&age=22&gender=male&phone=1013";
         HttpResponse<String> jsonresponse = Unirest.post(url).asObject(String.class);
-        String str = jsonresponse.getBody();
-        System.out.println(str);
+        JSONObject jsonObject = new JSONObject(jsonresponse.getBody());
+        System.out.println(jsonObject);
+        System.out.println(jsonObject.get("passengerId"));
+        PassengerId = jsonObject.get("passengerId").toString();
+        System.out.println(PassengerId);
         Assert.assertEquals(HttpStatus.OK.value(),jsonresponse.getStatus());
 
     }
@@ -57,7 +60,8 @@ public class PassengerControllerTest {
     @Test
     // Positive test for displaying passenger
     public void displayPassenger_pos() throws UnirestException {
-        HttpResponse<String> jsonresponse = Unirest.get("http://localhost:8888/passenger/26cc83af-9443-4ec7-8913-945d3f983f0d").asObject(String.class);
+        HttpResponse<String> jsonresponse = Unirest.get("http://localhost:8888/passenger/"+PassengerId).asObject(String.class);
+        System.out.println(PassengerId);
         Assert.assertEquals(HttpStatus.OK.value(),jsonresponse.getStatus());
 
     }
@@ -71,7 +75,7 @@ public class PassengerControllerTest {
     }
     @Test
     public void updatePassenger() throws UnirestException {
-        String url = "http://localhost:8888/passenger/ee99d5c9-bb1a-44*a9-a5ad-a4d103999db9?firstname=XX&lastname=YY&age=11&gender=famale&phone=123";
+        String url = "http://localhost:8888/passenger/"+PassengerId+"?firstname=XX&lastname=YY&age=11&gender=famale&phone=123";
         HttpResponse<String> jsonresponse = Unirest.put(url).asObject(String.class);
         Assert.assertEquals(HttpStatus.OK.value(),jsonresponse.getStatus());
     }
@@ -80,11 +84,14 @@ public class PassengerControllerTest {
     // Deleting passenger if passenger exists
     public void deletePassenger_pos() throws UnirestException {
         //createPassenger();
-        HttpResponse response = Unirest.delete("http://localhost:8888/passenger/aecb25dc-5d7f-49de-ba26-41eb3badc9c3").asString();
+
+        HttpResponse response = Unirest.delete("http://localhost:8888/passenger/962d6980-c980-4cf3-85a3-73e7e6e85c67").asString();
 //        HttpRequestWithBody response = Unirest.delete("http://localhost:8888/flight/121");
+        System.out.println(response);
         int status = response.getStatus();
         System.out.println(status);
-        Assert.assertEquals(status,400);
+        System.out.println(PassengerId);
+        Assert.assertEquals(status,200);
     }
 
 
